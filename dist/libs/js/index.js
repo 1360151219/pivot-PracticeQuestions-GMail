bef6a6b1c2869ac360901f5cd2e0044651722916
ZJX <1360151219@qq.com>
2021-04-10 13:05:56 +0800

    第四次提交:彻底完成写邮件功能和部分邮件操作功能

--- dist/libs/js/index.js
+++ dist/libs/js/index.js
@@ -4,6 +4,11 @@ const mailClose = document.getElementById("mail-close");
 const textMenuOpen = document.getElementById("textMenu-open");
 const textMenu = document.getElementById("text-menu");
 const resizeWin = document.getElementById("resize-window");
+const tips = document.getElementsByClassName("tip");
+const listOPen = document.getElementById("list-open");
+const elseListOPen = document.getElementById("elseList-open");
+const nonRead = document.getElementById("non-read");
+const weidus = document.getElementsByName("weidu");
 /* 拖拽效果 */
 function drag(node) {
   node.onmousedown = function (ev) {
@@ -35,6 +40,7 @@ function drag(node) {
     document.onmousemove = null;
   };
 }
+drag(newMail);
 //封装一个获取随机颜色的函数
 function randomColor() {
   let str =
@@ -47,6 +53,7 @@ function randomColor() {
     ",1)";
   return str;
 }
+/* 编辑菜单探入弹出 */
 function openTextMenu() {
   let temp = false;
   textMenuOpen.onclick = function () {
@@ -59,6 +66,41 @@ function openTextMenu() {
     }
   };
 }
+/* 保存功能函数 */
+var Download = function (content, filename) {
+  // 创建隐藏的可下载链接
+  var eleLink = document.createElement("a");
+  eleLink.download = filename;
+  eleLink.style.display = "none";
+  // 字符内容转变成blob地址
+  var blob = new Blob([content]);
+  eleLink.href = URL.createObjectURL(blob);
+  // 触发点击
+  document.body.appendChild(eleLink);
+  eleLink.click();
+  // 然后移除
+  document.body.removeChild(eleLink);
+};
+// 单个邮件的移入移出
+function mailFunc() {
+  const details = document.getElementsByClassName("detail");
+  const mailBody = document.getElementsByClassName("mail-item");
+  const date = document.getElementsByClassName("date");
+  const handle = document.getElementsByClassName("handle");
+  for (let i = 0; i < mailBody.length; i++) {
+    mailBody[i].onmouseenter = function () {
+      date[i].style.display = "none";
+      handle[i].style.display = "block";
+      details[i].style.width = "912px";
+    };
+    mailBody[i].onmouseleave = function () {
+      date[i].style.display = "block";
+      handle[i].style.display = "none";
+      details[i].style.width = "1002px";
+    };
+  }
+}
+mailFunc();
 /* 侧边栏移入移出效果 */
 (function () {
   const leftAside = document.getElementById("left-aside");
@@ -85,7 +127,7 @@ function openTextMenu() {
     footerList.style.display = "none";
   };
 })();
-drag(newMail);
+
 /* 写邮件功能函数 */
 (function () {
   //写邮件窗口的打开关闭;
@@ -129,4 +171,116 @@ drag(newMail);
   fontColor.onclick = function () {
     textArea.style.color = randomColor();
   };
+  /* 保存至本地 */
+  const textSave = document.getElementById("text-save");
+  textSave.onclick = function () {
+    Download(textArea.innerText, "test.txt");
+  };
+})();
+/* tip的实现 */
+(function () {
+  for (let i = 0; i < tips.length; i++) {
+    tips[i].parentNode.onmouseenter = function () {
+      setTimeout(() => {
+        tips[i].style.display = "block";
+      }, 100);
+    };
+    tips[i].parentNode.onmouseleave = function () {
+      tips[i].style.display = "none";
+    };
+  }
+})();
+/* 未读和其他邮件的展开 */
+(function () {
+  let temp_noneread = false;
+  let temp_else = false;
+  listOPen.onclick = function () {
+    if (!temp_noneread) {
+      listOPen.parentNode.nextElementSibling.style.display = "block";
+      temp_noneread = true;
+    } else {
+      listOPen.parentNode.nextElementSibling.style.display = "none";
+      temp_noneread = false;
+    }
+  };
+  elseListOPen.onclick = function () {
+    if (!temp_else) {
+      elseListOPen.parentNode.nextElementSibling.style.display = "block";
+      temp_else = true;
+    } else {
+      elseListOPen.parentNode.nextElementSibling.style.display = "none";
+      temp_else = false;
+    }
+  };
+})();
+
+const checkAll = document.getElementById("check-all");
+const checkmails = document.getElementsByName("checkmail");
+/* 全选按钮功能实现 */
+(function () {
+  checkAll.onclick = function () {
+    if (checkAll.checked == true) {
+      checkAll.checked = false;
+      checkAll.setAttribute("class", "iconfont icon-checkboxoutlineblank");
+      for (let i = 0; i < checkmails.length; i++) {
+        checkmails[i].checked = false;
+        checkmails[i].setAttribute(
+          "class",
+          "iconfont icon-checkboxoutlineblank checkmail"
+        );
+      }
+    } else {
+      checkAll.checked = true;
+      checkAll.setAttribute("class", "iconfont icon-check_box-px");
+      for (let i = 0; i < checkmails.length; i++) {
+        checkmails[i].checked = true;
+        checkmails[i].setAttribute(
+          "class",
+          "iconfont icon-check_box-px checkmail"
+        );
+      }
+    }
+  };
+})();
+/* 单个check功能 */
+(function () {
+  let num = 0;
+  for (let i = 0; i < checkmails.length; i++) {
+    checkmails[i].onclick = function () {
+      if (checkmails[i].checked) {
+        checkmails[i].checked = false;
+        num--;
+        checkmails[i].setAttribute(
+          "class",
+          "iconfont icon-checkboxoutlineblank checkmail"
+        );
+        if (num != checkmails.length) {
+          checkAll.checked = false;
+          checkAll.setAttribute("class", "iconfont icon-checkboxoutlineblank");
+        }
+      } else {
+        checkmails[i].checked = true;
+        num++;
+        checkmails[i].setAttribute(
+          "class",
+          "iconfont icon-check_box-px checkmail"
+        );
+        if (num == checkmails.length) {
+          checkAll.checked = true;
+          checkAll.setAttribute("class", "iconfont icon-check_box-px");
+        }
+      }
+    };
+  }
+})();
+/* 邮件从其他移入未读 */
+(function () {
+  for (let i = 0; i < weidus.length; i++) {
+    weidus[i].onclick = function () {
+      let oneMail = weidus[i].parentNode.parentNode.parentNode;
+      nonRead.childNodes[1].appendChild(oneMail);
+      this.innerHTML = `<div class="tip tip-2">已读</div>`;
+      mailFunc();
+    };
+  }
 })();
